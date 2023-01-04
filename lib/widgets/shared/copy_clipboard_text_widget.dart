a13596de63d097fffccd7c618c35dcbaea219c7f
barry-ruprai <b.code@tutamail.com>
2023-01-04 14:07:50 -0500

    v2.2.83+130
    Updated pay.cool rewards and transaction history UI

---
+++ lib/widgets/shared/copy_clipboard_text_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:paycool/constants/colors.dart';
+import 'package:paycool/service_locator.dart';
+import 'package:paycool/services/shared_service.dart';
+
+class CopyClipboardTextWidget extends StatelessWidget {
+  final String text;
+  const CopyClipboardTextWidget(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    final sharedService = locator<SharedService>();
+    return CupertinoButton(
+        child: const Icon(
+          FontAwesomeIcons.copy,
+          color: primaryColor,
+          size: 16,
+        ),
+        onPressed: () {
+          sharedService.copyAddress(context, text)();
+        });
+  }
+}
